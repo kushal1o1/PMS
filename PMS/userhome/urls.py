b98bd866f5aee596cb3425f770b1cf53304df683
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-05-07 19:16:04 +0545

    Added: showBills and showVaccine Page,view,url

--- PMS/userhome/urls.py
+++ PMS/userhome/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('mainpage/<int:user_id>/', views.userHome, name='mainpage'),
     path('mainpage/<int:user_id>/submit_detail/', views.submit_detail, name='submit_detail'),
     path('mainpage/<int:user_id>/profile/submit_bill/<str:poultryName>', views.submit_bill, name='submit_bill'),
+    path('mainpage/<int:user_id>/profile/bills/<str:poultryName>',views.showBills, name='showBills'),
+    path('mainpage/<int:user_id>/profile/vaccine/<str:poultryName>',views.showVaccine, name='showVaccine'),
     
 ]
